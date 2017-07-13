39ead4b06567e20037a8cebca73c2abfe2c4cebc
nikogu <644506165@qq.com>
2017-07-13 16:58:03 +0800

    添加 hmr eslint

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import AntdComment from '../src';
-
 import 'moment/locale/zh-cn';
+
+import AntdComment from '../src';
 
 class App extends Component {
   constructor(props) {
