9dfd199a53e5c9290ef0172ec7dba3c168f3b82b
nikogu <644506165@qq.com>
2017-07-13 16:53:02 +0800

    add eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,20 @@ const path = require('path');
 const glob = require('glob');
 
 module.exports = function (webpackConfig, env) {
+
   webpackConfig.babel.babelrc = false;
   webpackConfig.babel.plugins.push('transform-runtime');
   webpackConfig.babel.plugins.push(['antd', {
     style: 'css'  // if true, use less
   }]);
+
+  webpackConfig.module.preLoaders = [
+    {
+      test: /\.jsx$|\.js$/,
+      loader: 'eslint',
+      exclude: /node_modules/,
+    },
+  ];
 
   // Parse all less files as css module.
   //webpackConfig.module.loaders.forEach(function (loader, index) {
